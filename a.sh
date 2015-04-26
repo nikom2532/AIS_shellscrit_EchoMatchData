141425ad20f4f62d5dd5520abf52e1c3099745f6
Arming Huang <arming@buildthedot.com>
2015-04-26 15:29:02 +0700

    Update

--- a.sh
+++ a.sh
@@ -4,15 +4,17 @@
 set start_line=`grep -n "start" input | sed 's/:.*//g'`
 
 set start_line=`expr ${start_line} + 1`
-set finish=0
+set finish="0"
 
 while ($finish == 1)
 	
 	set data=`cat input | sed -n "${start_line}p"`
 	
-	if ( $data != "" )
-		#set data=`cat input`
-	end
+	if ( $data != "" ) then
+		@ finish = "1"
+	else
+		@ start_line=`expr ${start_line} + 1`
+	endif
 end
 
 
