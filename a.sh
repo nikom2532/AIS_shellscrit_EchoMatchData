f18433220ae47a012609587ca939e43c5e0a89d5
Arming Huang <arming@buildthedot.com>
2015-04-26 16:01:15 +0700

    Add Feature End Line

--- a.sh
+++ a.sh
@@ -16,3 +16,20 @@ while ($finish == 0)
 end
 
 echo "${start_line}"
+
+set end_line=`expr ${start_line} + 1`
+@ finish="0"
+
+while ($finish == 0)
+	set data=`cat input | sed -n "${end_line}p"`
+	
+	if ( $data == "exit" ) then
+		@ finish = "1"
+	else
+		@ end_line=`expr ${end_line} + 1`
+	endif
+end
+
+echo "${end_line}"
+
+
